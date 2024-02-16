59fbbe609a0dd726cfecca657724cc743c9b6118
Brett <brettgraham@gmail.com>
2024-02-16 17:44:07 -0500

    test

--- asdf_zarr/extensions.py
+++ asdf_zarr/extensions.py
@@ -8,6 +8,11 @@ class ZarrExtension(asdf.extension.Extension):
     tags = ["asdf://stsci.edu/example-project/tags/zarr-1.0.0"]
     converters = [ZarrConverter()]
 
+class ZarrExtensionV2(asdf.extension.Extension):
+    extension_uri = "asdf://stsci.edu/example-project/tags/zarr-2.0.0"
+    tags = ["asdf://stsci.edu/example-project/tags/zarr-2.0.0"]
+    converters = [ZarrConverter()]
+
 
 def get_extensions():
-    return [ZarrExtension()]
+    return [ZarrExtensionV2(), ZarrExtension()]
